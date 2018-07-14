e75736c7fc5dde51149160b4402703c012a862d7
Gabriel Vance <ilovenarwhales@gmail.com>
2018-07-14 09:26:42 +0300

    typscript

---
+++ week-1/es5-typescript-example/Vance-1.4.js
@@ -0,0 +1,6 @@
+var firstName = 'Gabriel';
+var lastName = 'Vance';
+function hello(firstName, lastName) {
+    return 'Hello ' + firstName + ' ' + lastName;
+}
+console.log(hello(firstName, lastName));
---
+++ week-1/es5-typescript-example/Vance-1.4.ts
@@ -0,0 +1,8 @@
+var firstName: string= 'Gabriel';
+var lastName: string= 'Vance';
+
+function hello(firstName, lastName) {
+    return 'Hello ' + firstName+ ' ' + lastName;
+    }
+
+console.log(hello(firstName, lastName));
