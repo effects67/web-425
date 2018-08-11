e4beeae68de78a7cdd2aa46015aa81c6eb5b2238
Gabriel Vance <ilovenarwhales@gmail.com>
2018-08-11 14:22:36 +0300

    6.4

--- week-6/onchange-events/src/app/app.component.ts
+++ week-6/onchange-events/src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-}
+  title = 'onChange() Example';
+
+  stock: string;
+  readonly numberOfShares = 100;
+
+  myGreeting = 'red';
+  myUser: { name: string } = { name: 'blue'}
+  }
+
--- week-6/onchange-events/src/app/child/child.component.ts
+++ week-6/onchange-events/src/app/child/child.component.ts
@@ -1,19 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-child',
   template: `
-    <p>
-      child works!
-    </p>
+    <div class="child">
+      <h2>Child</h2>
+      <div>Roses are: {{ greeting }}</div>
+      <div>Violets are: {{ user.name }}</div>
+    </div>
   `,
-  styles: []
+  styles: [`
+  `]
 })
-export class ChildComponent implements OnInit {
+export class ChildComponent implements OnChanges {
+
+  @Input() greeting: string;
+  @Input() user: { name: string };
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(JSON.stringify(changes, null, 2));
   }
-
 }
